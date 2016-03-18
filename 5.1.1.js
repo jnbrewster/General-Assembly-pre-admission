c93177b35fdac64935a0ad8e49b61b35c73ef182
Joel Brewster <jnbrewster@fastmail.com>
2016-03-18 12:35:09 +1100

    Formatting and refactoring

--- 5.1.1.js
+++ 5.1.1.js
@@ -1,14 +1,12 @@
 // 5.1.1
-// sayHello, which accepts one parameter, called name; it should return a greeting to a person with that name, i.e. "Hello, name".
-// areBothEven, which accepts two parameters, a and b; it should return true if a and b are both even, but false otherwise.
-// hotOrNot, which accepts one parameter, temp; it should return "hot" if temp is above 75, but should return "not hot" otherwise.
-// threeIfNull, which accepts one parameter,num. If num is null, it should return 3, but otherwise, it should return the value of num.
-// greatest, which accepts three parameters (x, y, and z), and returns the one with the greatest value. For example, greatest(5, 10, 1) would evaluate to 10.
 
+// sayHello, which accepts one parameter, called name; it should return a greeting to a person with that name, i.e. "Hello, name".
 var sayHello = function(name) {
   return "Hello, " name + ".";
 };
 
+
+// areBothEven, which accepts two parameters, a and b; it should return true if a and b are both even, but false otherwise.
 var areBothEven = function(a, b) {
   if (a % 2 === 0 && b % 2 === 0) {
     return true;
@@ -18,6 +16,13 @@ var areBothEven = function(a, b) {
   }
 };
 
+// Refactored
+var areBothEven = function(a, b) {
+  return a%2 === 0 && b%2 === 0;
+}
+
+
+// hotOrNot, which accepts one parameter, temp; it should return "hot" if temp is above 75, but should return "not hot" otherwise.
 var hotOrNot = function(temp) {
   if (temp > 75) {
     return "Hot";
@@ -27,6 +32,11 @@ var hotOrNot = function(temp) {
   }
 };
 
+// Refactored
+// TODO
+
+
+// threeIfNull, which accepts one parameter,num. If num is null, it should return 3, but otherwise, it should return the value of num.
 var threeIfNull = function(num) {
   if (num == null) {
     return 3;
@@ -36,6 +46,12 @@ var threeIfNull = function(num) {
   }
 };
 
+//refactored
+var threeIfNull = function(num) {
+  return num || 3;
+}
+
+// greatest, which accepts three parameters (x, y, and z), and returns the one with the greatest value. For example, greatest(5, 10, 1) would evaluate to 10.
 var greatest = function(x, y, x) {
   greatest = a;
 
